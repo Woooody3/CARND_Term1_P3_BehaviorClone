7d584b03866c85d041b48782a5730b6d9cffe929
Woooody Amadeus <woooody3@gmail.com>
2017-08-31 18:00:06 -0700

    Update clone.py 	- rule out 70% of data with near zero steering 	- add generator function 	- adjust image color 	- update lenet model to nvidia model
    update model.h5
    	- correspond to newer version of clone.py

--- clone.py
+++ clone.py
@@ -1,76 +1,113 @@
 import csv
 import cv2
 import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
+from random import shuffle
+import matplotlib.pyplot as plt 			
 
+### Preparing Data ###
+
+# Import data
+# and drop off about 70% of data in straight road with no steering, to avoid bias
 lines = []
-with open('data/driving_log.csv') as csvfile:
+
+with open('0831_1/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
+		probability = np.random.random()
+		if probability < 0.5 and float(line[3]) <0.005:
+			continue
 		lines.append(line)
-#	for row in reader:
-#		steering_center = float(row[3])
-#		# create adjusted steering measurements
-#		correction = 0.2
-#		steering_left = steering_center + correction
-#		steering_right = steering_center - correction
-#		# read images from left, center and right
-#		path = 'data/IMG/'
-#		img_center = process_img(np.asarray(Image.open(path+row[0])))
-#		img_left = process_img(np.asarray(Image.open(path+row[1])))
-#		img_right = process_img(np.asarray(Image.open(path+row[2])))
-#		# add images and angles to dataset
-#		car_images.extend(img_center, img_left, img_right)
-#		car_angles.extend(steering_center, steering_left, steering_right)
+
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+
+# Define a input data generator, to deal with large volume of collected image data.
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+	while 1: 
+		shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset : offset+batch_size]
+
+			images = []
+			measurements = []
+			for batch_sample in batch_samples:
+				for i in range(3): # append center, left, right images and corresponding measurements
+					image_path = '0831_1/IMG/' + batch_sample[i].split('/')[-1]
+					img = cv2.imread(image_path)
+					image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+					images.append(image)
+					measurement = float(batch_sample[3]) + 0.2 * (5*i - 3*i*i) /2	# if i=0, measurement is center; if i=1, measurement is +0.2 correction, if i=2, measurement is -0.2 correction.
+					measurements.append(measurement)
+			
+			# adding the fliped images and measurements
+			augmented_images, augmented_measurements = [], []
+			for image, measurement in zip(images, measurements):
+				augmented_images.append(image)
+				augmented_measurements.append(measurement)
+				augmented_images.append(cv2.flip(image, 1))
+				augmented_measurements.append(measurement * (-1))
+
+			X_in = np.array(augmented_images)
+			y_in = np.array(augmented_measurements)
+			yield sklearn.utils.shuffle(X_in, y_in)
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 
-images = []
-measurements = []
-correction = 0.2
-for line in lines:
-	for i in range(3):
-		source_path = line[i]
-		filename = source_path.split('/')[-1]
-		current_path = 'data/IMG/' + filename
-		image = cv2.imread(current_path)
-		images.append(image)
-		measurement = float(line[3]) + correction * (5*i - 3*i*i) /2	# if i=0, measurement is center; if i=1, measurement is +0.2 correction, if i=2, measurement is -0.2 correction.
-		measurements.append(measurement)
+### Training Model ###
 
-'''
-Follow 6 lines are learnt from course vedio, 
-actually I don't understand the differences to 
-add augmented data in "for line in lines" cycle.
-'''
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-	augmented_images.append(image)
-	augmented_measurements.append(measurement)
-	augmented_images.append(cv2.flip(image, 1))
-	augmented_measurements.append(measurement * (-1))
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+def img_resize(X):
+	from keras.backend import tf as ktf
+	return ktf.image.resize_images(X, (60,60))
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+dropping_prob = 0.60
 model = Sequential()
-model.add(Cropping2D(cropping=(75,25),(0,0), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((75,25),(0,0)), input_shape=(160,320,3)))
+model.add(Lambda(img_resize))
 model.add(Lambda(lambda x: (x/255.0)-0.5))
-model.add(Convolution2D(6,5,5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24,2,2, subsample=(2,2), activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Dropout(dropping_prob))
+model.add(Convolution2D(36,2,2, subsample=(2,2), activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Dropout(dropping_prob))
+model.add(Convolution2D(48,2,2, subsample=(2,2), activation='relu'))
+model.add(Dropout(dropping_prob))
+model.add(Convolution2D(64,2,2, activation='relu'))
+model.add(Convolution2D(64,2,2, activation='relu'))
 model.add(Flatten())
 model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5) 
+
+# Visualizing Loss
+'''
+Confused: what's the better value of the samples_per_epoch?
+'''
+history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), 
+	validation_data=validation_generator, nb_val_samples=len(validation_samples), 
+	nb_epoch=6, verbose=1)
+print(history_object.history.keys())
+
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('Model MSE loss')
+plt.ylabel('MeanSquareError')
+plt.xlabel('epoch')
+plt.legend(['training_set', 'validation_set'], loc='upper right')
+plt.show()
 
 model.save('model.h5')
 exit()
